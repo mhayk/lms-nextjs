9c53caa12e8696e4588c8ba4501e70d4078440c8
Mhayk Whandson <mhayk@kaumediagroup.com>
2025-02-25 15:41:05 +0000

    feat: installed shadcn and configure the dark/light theme

---
+++ components/Header.tsx
@@ -0,0 +1,12 @@
+import { DarkModeToggle } from "./DarkModeToggle"
+import { Button } from "./ui/button"
+
+function Header() {
+  return (
+    <div>
+        <DarkModeToggle />
+    </div>
+  )
+}
+
+export default Header
