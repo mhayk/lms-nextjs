bd7c60a15d542fa8f5f31b9b940b20cf059666e5
Mhayk Whandson <mhayk@kaumediagroup.com>
2025-03-10 12:08:32 +0000

    feat: create the search input component

---
+++ components/SearchInput.tsx
@@ -0,0 +1,40 @@
+'use client'
+
+import { Search } from "lucide-react"
+import { useRouter } from "next/navigation"
+import { FormEvent, useState } from "react"
+
+function SearchInput() {
+    const router = useRouter()
+    const [searchQuery, setSearchQuery] = useState("")
+
+    const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+        e.preventDefault()
+
+        if (searchQuery.trim()) {
+            router.push(`/search/${encodeURIComponent(searchQuery.trim())}`)
+        }
+    }
+
+    return (
+        <form
+            className="relative w-full flex-1 max-w-[300px]"
+            onSubmit={handleSubmit}
+        >
+            <input
+                type="text"
+                placeholder="Search courses..."
+                value={searchQuery}
+                onChange={(e) => setSearchQuery(e.target.value)}
+                className="w-full rounded-full bg-secondary/80 px-4 py-2 pl-10 text-sm
+                focus:outline-none focus:ring-2 focus:ring-primary
+                "
+            />
+            <Search className="absolute left-3 top-1/2 h-4 w-4 -translate-y-1/2
+            text-muted-foreground
+            " />
+        </form>
+    )
+}
+
+export default SearchInput
