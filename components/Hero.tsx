5bb538caca2dc75af312156a48be2e3ba387f9b4
Mhayk Whandson <mhayk@kaumediagroup.com>
2025-03-18 15:30:43 +0000

    feat: create the schema types and some other blocks too

---
+++ components/Hero.tsx
@@ -0,0 +1,25 @@
+import React from 'react'
+
+function Hero() {
+    return (
+        <div className='relative h-[45vh] w-full'>
+            <div className="absolute inset-0 bg-gradient-to-b from-black/10 to-bloack/55 dark:from-white/15 dark:to-black/40"></div>
+            <div className="absolute inset-0 bg-gradient-to-t from-background via-background/80 to-background/20"></div>
+
+            <div className="relative container mx-auto px-4 h-full flex flex-col justify-center">
+                <div className="max-w-3xl">
+                    <h1 className='text-5xl md:text-6xl font-bold mb-4 bg-gradient-to-r from-foreground
+                    to-foreground/80 bg-clip-text text-transparent'>
+                        Expand Your knowledge with Our Courses
+                    </h1>
+                    <p className="text-xl text-muted-foreground">
+                        Discover a world of learning with our expertly crafted courses.
+                        Lead from industry professionals and take your skills to the next level.
+                    </p>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Hero
---
+++ sanity/schemaTypes/studentType.tsx
@@ -0,0 +1,56 @@
+import Image from 'next/image';
+import { defineField, defineType } from 'sanity';
+
+export const studentType = defineType({
+    name: "student",
+    title: "Student",
+    type: "document",
+    fields: [
+        defineField({
+            name: "firstName",
+            title: "First Name",
+            type: "string",
+        }),
+        defineField({
+            name: "lastName",
+            title: "Last Name",
+            type: "string",
+        }),
+        defineField({
+            name: "email",
+            title: "Email",
+            type: "string",
+            validation: (rule) => rule.required(),
+        }),
+        defineField({
+            name: "clerkiId",
+            title: "Clerki User ID",
+            type: "string",
+            validation: (rule) => rule.required(),
+        }),
+        defineField({
+            name: "imageUrl",
+            title: "Profile Image URL",
+            type: "string",
+        }),
+    ],
+    preview: {
+        select: {
+            firstName: "firstName",
+            lastName: "lastName",
+            imageUrl: "imageUrl",
+        },
+        prepare({ firstName, lastName, imageUrl }) {
+            return {
+                title: `${firstName.charAt(0).toUpperCase()}${firstName.slice(1)} ${lastName.charAt(0).toUpperCase()}${lastName.slice(1)}`,
+                media: (<Image
+                    src={imageUrl}
+                    alt={`${firstName} ${lastName}`}
+                    width={100}
+                    height={100}
+                />
+                ),
+            };
+        },
+    },
+})
